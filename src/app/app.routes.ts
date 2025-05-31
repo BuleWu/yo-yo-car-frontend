a6f946de99cae2f9ba8275215b0815980f69dcf6
Josip Bulić Lokalni Admin <josip.bulic@asee.io>
2025-05-31 18:50:13 +0200

    add_landing_page_route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {ROUTES} from './shared/enums/router.enum';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: ROUTES.LANDING_PAGE,
+    component: HomePageComponent
+  }
+];
---
+++ src/app/shared/enums/router.enum.ts
@@ -0,0 +1,7 @@
+/**
+ * Main Routes Enum
+ */
+
+export enum ROUTES {
+  LANDING_PAGE = 'welcome'
+}
