f91b40d8acbec53b3c8467f9ce7227a686d8ad30
Josip Bulić Lokalni Admin <josip.bulic@asee.io>
2025-06-01 14:51:30 +0200

    Add feature item component

---
+++ src/app/home-page/components/feature-item/feature-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-item',
+  imports: [],
+  templateUrl: './feature-item.component.html',
+  styleUrl: './feature-item.component.scss'
+})
+export class FeatureItemComponent {
+
+}
