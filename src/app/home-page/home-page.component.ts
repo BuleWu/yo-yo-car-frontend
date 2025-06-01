08db96229d8cdc0d906511cc2b0a6ad6aadfab28
Josip Bulić Lokalni Admin <josip.bulic@asee.io>
2025-06-01 17:06:39 +0200

    Minor changes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {FeatureItemComponent} from './components/feature-item/feature-item.component';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
   imports: [
-    FeatureItemComponent
+    FeatureItemComponent,
+    MatButton
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
@@ -18,22 +20,22 @@ export class HomePageComponent {
       backgroundColor: '#EFEAFF',
     },
     {
-      name: 'Search for rides',
-      description: 'You can look for rides from an to any location',
-      imgPath: '../../assets/images/search-rides.svg',
-      backgroundColor: '#EFEAFF',
+      name: 'Save money',
+      description: 'Reduce emissions, and meet new people',
+      imgPath: '../../assets/images/save-money.svg',
+      backgroundColor: '#ECFFDA',
     },
     {
-      name: 'Search for rides',
-      description: 'You can look for rides from an to any location',
-      imgPath: '../../assets/images/search-rides.svg',
-      backgroundColor: '#EFEAFF',
+      name: 'Post your own ride',
+      description: 'Share your journey with others',
+      imgPath: '../../assets/images/ride-share.svg',
+      backgroundColor: '#DAE6FF',
     },
     {
-      name: 'Search for rides',
-      description: 'You can look for rides from an to any location',
-      imgPath: '../../assets/images/search-rides.svg',
-      backgroundColor: '#EFEAFF',
+      name: 'Trust your fellow travelers',
+      description: 'We verify ratings, profiles, and IDs to ensure you know who you’re riding with and who can safely book',
+      imgPath: '../../assets/images/check-circle.svg',
+      backgroundColor: '#FFE5DA',
     },
 
   ]
