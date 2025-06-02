f9c0d3db8667f32a463aaf4bbcc9d59df69779c6
Josip Bulić Lokalni Admin <josip.bulic@asee.io>
2025-06-02 22:59:28 +0200

    Add login page background

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import {Routes} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
-import {ROUTES} from './shared/enums/router.enum';
+import {LoginComponent} from './features/auth/login/login.component';
+/*import {ROUTES} from './shared/enums/router.enum';*/
 
 export const routes: Routes = [
   {
     path: 'welcome',
     /*redirectTo: ROUTES.LANDING_PAGE,*/
     component: HomePageComponent
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent
   }
 ];
---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#050A24';
+  }
+}
