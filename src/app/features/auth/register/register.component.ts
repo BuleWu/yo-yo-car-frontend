290c5117b1c34d657b625d20a2b4c665ad6b9416
Josip Bulić Lokalni Admin <josip.bulic@asee.io>
2025-06-04 14:42:55 +0200

    Overwrite outline color, edit buttons

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatFormField,
+    MatButton
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
