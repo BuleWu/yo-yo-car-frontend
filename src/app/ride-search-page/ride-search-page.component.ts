32bd887c6ff46f1b37f736312d4755e21eae1f7f
Josip Bulić <josip.bulic@asee.io>
2025-07-25 19:00:43 +0200

    Add not found design

--- src/app/ride-search-page/ride-search-page.component.ts
+++ src/app/ride-search-page/ride-search-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NavbarComponent} from '../shared/components/navbar/navbar.component';
 import {RideSearchComponent} from '../features/rides/components/ride-search/ride-search.component';
 import {RideProviderService} from '../features/rides/services/ride-provider-service/ride-provider.service';
@@ -8,7 +8,9 @@ import {Ride} from '../shared/models/ride/ride-models';
 import {AsyncPipe} from '@angular/common';
 import {RideCardComponent} from './components/ride-card/ride-card.component';
 import {FooterComponent} from '../shared/components/footer/footer.component';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'app-ride-search-page',
   imports: [
@@ -21,7 +23,7 @@ import {FooterComponent} from '../shared/components/footer/footer.component';
   templateUrl: './ride-search-page.component.html',
   styleUrl: './ride-search-page.component.scss'
 })
-export class RideSearchPageComponent implements OnInit {
+export class RideSearchPageComponent implements OnInit, OnDestroy {
   public rides$: Observable<Ride[]> = of([]);
   public startingPoint: string = '';
   public destination: string = '';
@@ -33,18 +35,24 @@ export class RideSearchPageComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.startingPoint = this._activatedRoute.snapshot.queryParamMap.get('starting_point') as string;
-    this.destination = this._activatedRoute.snapshot.queryParamMap.get('destination') as string;
+    this._activatedRoute.queryParams
+      .pipe(untilDestroyed(this))
+      .subscribe((params) => {
+        this.startingPoint = params['starting_point'];
+        this.destination = params['destination'];
 
-    this.rides$ = this._rideProviderService.searchRides([
-      {
-        filter: 'starting_point',
-        value: this.startingPoint
-      },
-      {
-        filter: 'destination',
-        value: this.destination
-      }
-    ])
+        this.rides$ = this._rideProviderService.searchRides([
+          {
+            filter: 'starting_point',
+            value: this.startingPoint
+          },
+          {
+            filter: 'destination',
+            value: this.destination
+          }
+        ])
+    })
   }
+
+  ngOnDestroy(): void {}
 }
