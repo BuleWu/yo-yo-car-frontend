6f0b7cad4a69bc4e4ba79d5b8b1775b6c372adaa
Josip Bulić <josip.bulic@asee.io>
2025-07-26 19:26:56 +0200

    Add user rating

--- src/app/ride-search-page/components/ride-card/ride-card.component.ts
+++ src/app/ride-search-page/components/ride-card/ride-card.component.ts
@@ -1,20 +1,35 @@
-import {Component, Input} from '@angular/core';
-import {User} from '../../../shared/models/user/user-models';
+import {Component, Input, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Ride} from '../../../shared/models/ride/ride-models';
+import {RatingProviderService} from '../../../features/ratings/services/rating-provider.service';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-ride-card',
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './ride-card.component.html',
   styleUrl: './ride-card.component.scss'
 })
-export class RideCardComponent {
+export class RideCardComponent implements OnInit {
   @Input() ride: Ride | undefined = undefined;
 
+  public userRating?: number;
+
   constructor(
-    private _router: Router
+    private _router: Router,
+    private _ratingProviderService: RatingProviderService,
   ) {
+  }
+
+  ngOnInit() {
+    this._ratingProviderService.getRatingsByUserId(this.ride?.driverId as string)
+      .pipe()
+      .subscribe((ratings) => {
+        const sum = ratings.reduce((acc, curr) => acc + curr.value, 0)
+        this.userRating = sum / ratings.length;
+      })
   }
 
   public viewRideDetails(): void {
