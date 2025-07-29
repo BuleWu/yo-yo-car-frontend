d4f80dab0fb3a25ca3dc2712e28356a0979e3d00
Josip Bulić <josip.bulic@asee.io>
2025-07-29 13:24:23 +0200

    Convert camel to snake case in requests

---
+++ src/app/http-interceptors/http.interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import {deepObjCamelToSnakeCase} from '../common/generic/utils/data-manipulation/deep-obj-camel-to-snake-case';
+
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+  let body = req.body;
+
+  if(body && typeof body == 'object') {
+    body = deepObjCamelToSnakeCase(body);
+  }
+
+  const modifiedReq = req.clone({ body })
+
+  return next(modifiedReq);
+};
