9fa6148929bda0eca9cfede8d809e65a5788d0de
Josip Bulić <josip.bulic@asee.io>
2025-07-31 11:44:26 +0200

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import {FindRidePageComponent} from './find-ride-page/find-ride-page.component';
 import {RideSearchPageComponent} from './ride-search-page/ride-search-page.component';
 import {RideInfoComponent} from './ride-info/ride-info.component';
 import {PostRideComponent} from './features/rides/components/post-ride/post-ride.component';
+import {UserProfileComponent} from './features/users/components/user-profile/user-profile.component';
 
 export const routes: Routes = [
   {
@@ -74,7 +75,9 @@ export const routes: Routes = [
   },
   {
     path: ROUTES.PROFILE,
-    children: []
+    component: UserProfileComponent,
+    canActivate: [authGuard],
+    /*children: []*/
   },
   {
     path:'**',
